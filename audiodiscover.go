8c816520de839378f7e69ee480e1058248d3d2a6
kalpio <piotr.lkalina@gmail.com>
2023-11-16 00:28:00 +0100

    Renamed files

--- audiodiscover.go
+++ audiodiscover.go
@@ -1,4 +1,4 @@
-package audiodevice
+package audiodiscover
 
 import (
 	"os/exec"
--- audiodiscover_windows.go
+++ audiodiscover_windows.go
@@ -1,6 +1,6 @@
 //go:build windows
 
-package audiodevice
+package audiodiscover
 
 func listDevicesSwitches() []string {
 	return []string{"-hide_banner", "-f", "dshow", "-list_devices", "true", "-i", "null"}
